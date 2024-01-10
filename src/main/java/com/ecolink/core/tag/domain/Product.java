db9241ce26a5e2a09d3b1c3156f9d1647932795a
hyeonhee <dlgusgml1314@naver.com>
2024-01-10 21:01:01 +0900

    Feat: Tag 관련 엔티티 추가

---
+++ src/main/java/com/ecolink/core/tag/domain/Product.java
@@ -0,0 +1,29 @@
+package com.ecolink.core.tag.domain;
+
+import com.ecolink.core.common.domain.BaseTimeEntity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Product extends BaseTimeEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotNull
+	private String name;
+
+	@NotNull
+	private Integer count;
+	
+}
