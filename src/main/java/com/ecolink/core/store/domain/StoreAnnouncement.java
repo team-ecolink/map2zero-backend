dd24840f3410c79c7045b1d56779178a99d84310
yujin <youjin010402@naver.com>
2024-01-13 15:31:35 +0900

    Feat: StoreAnnouncement 필드 구체화

--- src/main/java/com/ecolink/core/store/domain/StoreAnnouncement.java
+++ src/main/java/com/ecolink/core/store/domain/StoreAnnouncement.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,13 +16,14 @@ import lombok.NoArgsConstructor;
 public class StoreAnnouncement extends BaseTimeEntity {
 
 	@Id
+	@NotNull
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	private String field;
+	@NotNull
+	private String title;
 
-	private String field2;
-
-	private String field3;
+	@NotNull
+	private String content;
 
 }
