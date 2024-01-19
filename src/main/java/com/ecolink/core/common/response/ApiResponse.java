07b9b5372f7d81cd7eff94f07e5970f4874d6a81
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2024-01-19 13:28:31 +0900

    Docs: `ApiResponse` 스웨거 문서 추가

--- src/main/java/com/ecolink/core/common/response/ApiResponse.java
+++ src/main/java/com/ecolink/core/common/response/ApiResponse.java
@@ -1,14 +1,18 @@
 package com.ecolink.core.common.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
 public class ApiResponse<T> {
 
+	@Schema(description = "요청한 응답에 대한 데이터")
 	private final T data;
 
+	@Schema(description = "HTTP 상태 코드", example = "200")
 	private final Integer status;
 
+	@Schema(description = "요청 관련 메세지", example = "OK")
 	private final String message;
 
 	private ApiResponse(T data, Integer status, String message) {
