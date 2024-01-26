2e0d019ed53ef9a0b24bca2f9a49af2ca0a5adf6
yujin <youjin010402@naver.com>
2024-01-26 22:25:23 +0900

    Refactor: 미사용 import 삭제

--- src/main/java/com/ecolink/core/bookmark/controller/BookmarkController.java
+++ src/main/java/com/ecolink/core/bookmark/controller/BookmarkController.java
@@ -1,7 +1,5 @@
 package com.ecolink.core.bookmark.controller;
 
-import java.util.Map;
-
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
