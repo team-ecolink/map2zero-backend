989a7ad0cc4959585570b34b73e064274f0f3359
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2024-01-29 23:26:48 +0900

    Feat: 오프셋 페이징 공통 응답 포맷 추가
    - 오프셋 페이징 공통 응답 포맷 클래스 `ApiPageResponse` 추가
    - 공통 응답 메세지 및 코드 타입 통일

--- src/main/java/com/ecolink/core/common/response/ApiResponse.java
+++ src/main/java/com/ecolink/core/common/response/ApiResponse.java
@@ -10,12 +10,12 @@ public class ApiResponse<T> {
 	private final T data;
 
 	@Schema(description = "HTTP 상태 코드", example = "200")
-	private final Integer status;
+	private final int status;
 
 	@Schema(description = "요청 관련 메세지", example = "OK")
 	private final String message;
 
-	private ApiResponse(T data, Integer status, String message) {
+	private ApiResponse(T data, int status, String message) {
 		this.status = status;
 		this.message = message;
 		this.data = data;
