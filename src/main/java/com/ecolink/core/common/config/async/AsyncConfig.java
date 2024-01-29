1d78a3126fb5236b7c3674a9819736ba03f8c7ad
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2024-01-29 23:42:01 +0900

    Refactor: JpaAuditing, 비동기 처리 설정 별도 분리

---
+++ src/main/java/com/ecolink/core/common/config/async/AsyncConfig.java
@@ -0,0 +1,9 @@
+package com.ecolink.core.common.config.async;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableAsync
+@Configuration
+public class AsyncConfig {
+}
