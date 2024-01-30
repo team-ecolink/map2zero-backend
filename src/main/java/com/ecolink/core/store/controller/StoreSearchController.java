3c5be12aa600f40247bb7e40daeec59549cf0464
Hyeon Hee <101989268+heegane@users.noreply.github.com>
2024-01-30 17:06:21 +0900

    Refactor: 반환 타입 `ApiCursorPageResponse`로 변경

--- src/main/java/com/ecolink/core/store/controller/StoreSearchController.java
+++ src/main/java/com/ecolink/core/store/controller/StoreSearchController.java
@@ -7,8 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ecolink.core.auth.token.UserPrincipal;
-import com.ecolink.core.common.dto.CursorPage;
-import com.ecolink.core.common.response.ApiResponse;
+import com.ecolink.core.common.response.ApiCursorPageResponse;
 import com.ecolink.core.common.util.AuthorityUtil;
 import com.ecolink.core.store.dto.request.StoreSearchRequest;
 import com.ecolink.core.store.dto.response.StoreSearchDto;
@@ -32,13 +31,13 @@ public class StoreSearchController {
 		description = "검색어가 포함된 매장을 조회하는 API - 인증 선택",
 		security = {@SecurityRequirement(name = "session-token")})
 	@GetMapping("/search")
-	public ApiResponse<CursorPage<StoreSearchDto, Long>> searchStores(
+	public ApiCursorPageResponse<StoreSearchDto, Long> searchStores(
 		@ParameterObject @Valid StoreSearchRequest request,
 		@AuthenticationPrincipal UserPrincipal principal) {
 
 		if (AuthorityUtil.hasUserAuthority(principal)) {
-			return ApiResponse.ok(storeSearchService.searchStores(request, principal.getAvatarId()));
+			return ApiCursorPageResponse.ok(storeSearchService.searchStores(request, principal.getAvatarId()));
 		}
-		return ApiResponse.ok(storeSearchService.searchStores(request, null));
+		return ApiCursorPageResponse.ok(storeSearchService.searchStores(request, null));
 	}
 }
