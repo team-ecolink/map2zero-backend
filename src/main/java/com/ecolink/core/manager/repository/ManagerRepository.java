0da9532ef26f1c1195ee34b760f4f679e7d43caf
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2024-02-11 20:33:13 +0900

    Feat: `GET /managers/stores` API 추가

--- src/main/java/com/ecolink/core/manager/repository/ManagerRepository.java
+++ src/main/java/com/ecolink/core/manager/repository/ManagerRepository.java
@@ -1,5 +1,6 @@
 package com.ecolink.core.manager.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,6 +9,7 @@ import org.springframework.data.repository.query.Param;
 
 import com.ecolink.core.manager.constant.ManagerStatus;
 import com.ecolink.core.manager.domain.Manager;
+import com.ecolink.core.store.domain.Store;
 import com.ecolink.core.user.domain.User;
 
 public interface ManagerRepository extends JpaRepository<Manager, Long> {
@@ -19,8 +21,15 @@ public interface ManagerRepository extends JpaRepository<Manager, Long> {
 	Optional<Manager> findByUser(@Param("user") User user);
 
 	@Query("select (count(m) > 0) from Manager m "
-		+ "where m.user.id = :id "
-		+ "and m.status = :status ")
-	boolean existsByUserAndStatus(@Param("id") Long id,@Param("status") ManagerStatus status);
+		   + "where m.user.id = :id "
+		   + "and m.status = :status ")
+	boolean existsByUserAndStatus(@Param("id") Long id, @Param("status") ManagerStatus status);
+
+	@Query("select s from Manager m "
+		   + "left join m.storeRegistrations sr "
+		   + "on sr.status = com.ecolink.core.manager.constant.RegistrationStatus.ACTIVE "
+		   + "left join sr.store s "
+		   + "where m.id = :managerId")
+	List<Store> findStoresByManager(@Param("managerId") Long id);
 
 }
