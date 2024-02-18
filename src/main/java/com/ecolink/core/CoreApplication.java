b39ee15d96e82f2db64751eab702b52c6f26292c
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2024-02-18 17:03:51 +0900

    Feat: 기본 타임존 설정 추가

--- src/main/java/com/ecolink/core/CoreApplication.java
+++ src/main/java/com/ecolink/core/CoreApplication.java
@@ -1,13 +1,22 @@
 package com.ecolink.core;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class CoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoreApplication.class, args);
+	}
+
+	@PostConstruct
+	public void setTimeZone() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 	}
 
 }
